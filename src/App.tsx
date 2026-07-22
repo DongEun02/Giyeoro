45ae42919b19e2403e910ff24873154a2a621f95
eunnn <ehddms0517@naver.com>
2026-07-22 19:48:14 +0900

    feat: expand landing page service overview

--- src/App.tsx
+++ src/App.tsx
@@ -1150,7 +1150,6 @@ export default function App() {
                 <strong>기여로</strong>
               </div>
               <div className="app-footer-links">
-                <a href="#">서비스 소개</a>
                 <a href="#">이용약관</a>
                 <a href="#">개인정보 처리방침</a>
                 <a href="#">문의하기</a>
